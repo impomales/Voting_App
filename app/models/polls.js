234c90d392f09e6be5c4e0a138df7d7feed08cc2
Isaias <impomales@gmail.com>
2016-10-07 05:33:46 +0000

    minor change to poll model

--- app/models/polls.js
+++ app/models/polls.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var Poll = new Schema({
+    title: String,
     createdBy: {type: Schema.Types.ObjectId, ref: 'Voter'},
     options: [{title: String, count: Number}]
 })
