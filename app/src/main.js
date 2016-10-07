a67578e482f5700feb191246900c6dfff55f0b1e
Isaias <impomales@gmail.com>
2016-10-07 04:42:11 +0000

    add react router

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var Redirect = require('react-router').Redirect;
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,
