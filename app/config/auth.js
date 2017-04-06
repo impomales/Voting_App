b2ef8f8d7ec0dd08717dde34e880443d1ad821e0
Isaias <impomales@gmail.com>
2017-04-06 16:55:14 -0400

    delete all files, redeveloping app

--- app/config/auth.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-	'facebookAuth': {
-		'clientID': process.env.FACEBOOK_KEY,
-		'clientSecret': process.env.FACEBOOK_SECRET,
-		'callbackURL': process.env.APP_URL + 'auth/facebook/callback'
-	}
-};
