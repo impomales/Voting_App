a33770510aebd7d43ac19e8fa30f1b09254b9a3e
Isaias <impomales@gmail.com>
2017-04-08 04:53:58 -0400

    add mongoose model

---
+++ models/models.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var userSchema = require('./schemas/user');
+var pollSchema = require('./schemas/poll');
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+var User = mongoose.model('User', userSchema, 'users');
+var Poll = mongoose.model('Poll', pollSchema, 'polls');
+
+var models = {
+	User: User,
+	Poll: Poll
+};
+
+module.exports = models;
