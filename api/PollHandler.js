1eca3b9b4c08e30fdc007f1995508323008d8cfe
Isaias <impomales@gmail.com>
2017-04-23 16:33:09 -0400

    can get all polls from database

--- api/PollHandler.js
+++ api/PollHandler.js
@@ -1,8 +1,21 @@
 var Poll = require('../models/models').Poll;
+var status = require('http-status');
 
 module.exports = function() {
 	this.getPolls = function(req, res) {
 		// get polls from server.
-		res.send('test');
+		Poll.
+			find({}).
+			exec(function(err, results) {
+				if (err) {
+					res.
+					status(status.INTERNAL_SERVER_ERROR).
+					json({ error: err.toString() });
+				}
+
+				var json = {};
+				json['polls'] = results;
+				res.json(json);
+			});
 	}
 }
