9194395c5abb19dbfa20f498b86419511ab713a9
Isaias <impomales@gmail.com>
2017-04-24 20:09:36 -0400

    add deletePoll function

--- api/PollHandler.js
+++ api/PollHandler.js
@@ -3,25 +3,25 @@ var status = require('http-status');
 
 module.exports = function() {
 	this.getMyPolls = function(req, res) {
-		// define.
+		// polls created by current user.
 		if (!req.user) {
 			return res.
-					status(status.UNAUTHORIZED).
-					json({ error: 'Not logged in.' });
+				status(status.UNAUTHORIZED).
+				json({ error: 'Not logged in.' });
 		}
 
 		Poll.
 			find({ created_by: req.user._id }).
-			exec(function(err, result) {
+			exec(function(err, results) {
 				if (err) {
-					res.
+					return res.
 						status(status.INTERNAL_SERVER_ERROR).
 						json({ error: err.toString() });
 				}
 
 				var json = {};
 				json['myPolls'] = results;
-				res.json(json);
+				return res.json(json);
 			});
 	};
 
@@ -31,14 +31,14 @@ module.exports = function() {
 			find({}).
 			exec(function(err, results) {
 				if (err) {
-					res.
+					return res.
 						status(status.INTERNAL_SERVER_ERROR).
 						json({ error: err.toString() });
 				}
 
 				var json = {};
 				json['polls'] = results;
-				res.json(json);
+				return res.json(json);
 			});
 	};
 
@@ -48,31 +48,58 @@ module.exports = function() {
 	};
 
 	this.getPollById = function(req, res) {
-		// define.
 		Poll.
 			findOne({ _id: req.params.id }).
 			exec(function(err, result) {
 				if (err) {
-					res.
+					return res.
 						status(status.INTERNAL_SERVER_ERROR).
 						json({ error: err.toString() });
 				}
 
 				if (!res) {
-					res.
+					return res.
 						status(status.NOT_FOUND).
 						json({ error: 'not found' });
 				}
 
 				var json = {};
 				json['poll'] = result;
-				res.json(json);
+				return res.json(json);
 			});
 	};
 
 	this.deletePoll = function(req, res) {
-		// define.
-		res.end();
+		// only can delete polls created by current user.
+		if (!req.user) {
+			return res.
+				status(status.UNAUTHORIZED).
+				json({ error: 'Not logged in.' });
+		}
+
+		Poll.
+			find({ _id: req.params.id }).
+			exec(function(err, result) {
+				if (err) {
+					return res.
+						status(status.INTERNAL_SERVER_ERROR).
+						json({ error: err.toString() });
+				}
+
+				if (result.created_by !== req.user._id) {
+					return res.
+						status(status.UNAUTHORIZED).
+						json({ error: 'unauthorized to delete this poll.' })
+				}
+
+				Poll.remove({ _id: req.params.id }, function(err) {
+					if (err) {
+						return res.
+						status(status.INTERNAL_SERVER_ERROR).
+						json({error: err.toString() });
+					}
+				});
+			});
 	};
 
 	this.addChoice = function(req, res) {
