a6ea79ffc7422bc9b16ba41ff92fd24194cee984
Isaias <impomales@gmail.com>
2017-05-12 17:03:30 -0400

    user can login/logout using google account

--- routes/routes.js
+++ routes/routes.js
@@ -1,24 +1,49 @@
 var PollHandler = require('../api/PollHandler');
+var path = process.cwd();
 
 module.exports = function(app) {
 	var pollHandler = new PollHandler();
 
+	function isLoggedIn(req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		} else {
+			res.redirect('/login');
+		}
+	}
+
+	app.route('/')
+		.get(isLoggedIn, function(req, res) {
+			res.sendFile(path + '/client/index.html');
+		});
+
+	app.route('/login')
+		.get(function(req, res) {
+			res.sendFile(path + '/client/login.html');
+		});
+
+	app.route('/logout')
+		.get(function(req, res) {
+			req.logout();
+			res.redirect('/login');
+		});
+
 	// get polls of current user.
 	app.route('/api/mypolls')
-		.get(pollHandler.getMyPolls);		//auth
+		.get(isLoggedIn, pollHandler.getMyPolls);		//auth
 
 	// get all polls.
 	app.route('/api/polls')
 		.get(pollHandler.getPolls)			//unauth
-		.post(pollHandler.addPoll);			//auth
+		.post(isLoggedIn, pollHandler.addPoll);			//auth
 
 	// get polls by id.
 	app.route('/api/polls/:id')
 		.get(pollHandler.getPollById)		//unauth
-		.delete(pollHandler.deletePoll);	//auth
+		.delete(isLoggedIn, pollHandler.deletePoll);	//auth
 
 	app.route('/api/choice/:id')
-		.put(pollHandler.addChoice);		//auth
+		.put(isLoggedIn, pollHandler.addChoice);		//auth
 
 	app.route('/api/vote/:id')
 		.put(pollHandler.vote);				//unauth
